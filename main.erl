f4e7f3592378f46ad81c2b3770383422b6a749fb
Isabel Lopez Murillo <isaana@email.unc.edu>
2025-02-25 15:59:40 -0500

    Some starter code (does not work)

--- main.erl
+++ main.erl
@@ -1,4 +1,42 @@
--module(main)
+-module(main).
 -compile([export_all]).
 
-start ->  io:format("Starting").
+start() ->  
+    io:format("Starting"),
+    serv1 = spawn(?MODULE, loop1, []),
+
+    
+    serv1 ! stop.
+
+loop1() -> 
+    % this is serv1
+    receive
+        {From, Msg} ->
+            io:format("(Serv1) received: ~w~n", [Msg]),
+            From ! {self(), Msg},
+            loop1();
+        stop ->
+            true
+    end.
+
+loop2() -> 
+    % this is serv2
+    receive
+        {From, Msg} ->
+            io:format("(Serv2) received: ~w~n", [Msg]),
+            From ! {self(), Msg},
+            loop2();
+        stop ->
+            true
+    end.
+
+loop3() -> 
+    % this is serv3
+    receive
+        {From, Msg} ->
+            io:format("(Serv3) received: ~w~n", [Msg]),
+            From ! {self(), Msg},
+            loop3();
+        stop ->
+            true
+    end.
